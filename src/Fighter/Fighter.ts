e1b4faad2b76c99dd5304a6513b7379462983111
Paulo Michael <paulo.michael@gmail.com>
2022-08-16 17:33:30 -0300

    feat: req06 - creates the `Fighter` interface

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,14 @@
+import IEnergy from '../Energy';
+
+interface IFighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: IEnergy;
+  attack(enemy: IFighter): void; 
+  special?(enemy: IFighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
+
+export default IFighter;
