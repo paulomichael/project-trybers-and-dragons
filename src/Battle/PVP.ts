fe2813b9c08c8263f491181eab21dde31322e50b
Paulo Michael <paulo.michael@gmail.com>
2022-08-17 13:54:32 -0300

    feat: req10 - creates the `PVP` class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,8 +2,27 @@ import Battle from './Battle';
 import Fighter from '../Fighter';
 
 class PVP extends Battle {
-  constructor(protected player: Fighter) {
-    super(player);   
+  private _player1: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player2);   
+    this._player1 = player1;
+  }
+
+  get player1(): Fighter {
+    return this._player1;
+  }
+
+  fight(): number {
+    while (this._player1.lifePoints !== -1 && this.player.lifePoints !== -1) {
+      this._player1.attack(this.player);
+      this.player.attack(this._player1);
+    }
+
+    if (this._player1.lifePoints === -1) {
+      return -1;
+    }
+    return 1;
   }
 }
 
