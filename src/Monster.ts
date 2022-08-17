ae2ebc2778eb9811879b44fc06d9996de4105e3a
Paulo Michael <paulo.michael@gmail.com>
2022-08-17 13:13:37 -0300

    feat: req09 - creates the `Monster` class

---
+++ src/Monster.ts
@@ -0,0 +1,36 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter | Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
